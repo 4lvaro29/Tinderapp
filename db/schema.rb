c29737e1d28baa3fd45ebab2f192d9b0e17a1f58
Alvaro Urbina <4lvaro.up@gmail.com>
2018-01-08 16:33:54 -0300

    model fixed with foreign key

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180103204703) do
+ActiveRecord::Schema.define(version: 20180108162110) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "interactions", force: :cascade do |t|
+    t.integer  "user_one_id"
+    t.integer  "user_two_id"
+    t.boolean  "like"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -35,4 +43,6 @@ ActiveRecord::Schema.define(version: 20180103204703) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "interactions", "users", column: "user_one_id"
+  add_foreign_key "interactions", "users", column: "user_two_id"
 end
